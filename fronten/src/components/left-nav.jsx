bbed0bdc52a327eca21978149ad0440b0d2e044b
yang <anyang47@hotmail.com>
2020-09-02 15:19:41 -0400

    admin home page layout

---
+++ fronten/src/components/left-nav.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./left-nav.css";
+
+const LeftNav = () => {
+  return <div className="left-nav">left nav</div>;
+};
+
+export default LeftNav;
--- fronten/src/pages/adminhome.js
+++ fronten/src/pages/adminhome.js
@@ -1,11 +1,26 @@
 import React from "react";
-import Cookies from 'js-cookie';
+import Cookies from "js-cookie";
+import { Layout } from "antd";
+import Header from "../components/header";
+import LeftNav from "../components/left-nav"
 
 function Adminhome() {
-  const userinfo = Cookies.get('userinfo')? JSON.parse(Cookies.get('userinfo')):{}
+  const { Footer, Sider, Content } = Layout;
+  const userinfo = Cookies.get("userinfo")
+    ? JSON.parse(Cookies.get("userinfo"))
+    : {};
   return (
     <>
-      Hello {userinfo.username}
+      <Layout style={{height:"100%"}}>
+        <Sider>
+            <LeftNav />
+        </Sider>
+        <Layout>
+          <Header />
+          <Content style={{backgroundColor:"#fff"}}>Content</Content>
+          <Footer style={{backgroundColor:"#ccccc", textAlign:"center"}}>Copy right reserved</Footer>
+        </Layout>
+      </Layout>
     </>
   );
 }
