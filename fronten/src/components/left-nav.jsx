7ca70101d2b80a9d2ca50f2167cfd272bb1cd468
yang <anyang47@hotmail.com>
2020-09-02 15:58:37 -0400

    static left nav menu

--- fronten/src/components/left-nav.jsx
+++ fronten/src/components/left-nav.jsx
@@ -1,8 +1,47 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./left-nav.css";
+import logo from "../pages/images/logo.jpg";
+import { Menu } from "antd";
+import {
+  AppstoreOutlined,
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  PieChartOutlined,
+  DesktopOutlined,
+  ContainerOutlined,
+  MailOutlined
+} from "@ant-design/icons";
+
+const { SubMenu } = Menu;
 
 const LeftNav = () => {
-  return <div className="left-nav">left nav</div>;
+  return (
+    <div to="/admin" className="left-nav">
+      {/* header */}
+      <Link to="/admin" className="left-nav-header">
+        <img src={logo} alt="" />
+        <h1>Back Office</h1>
+      </Link>
+      {/* header end */}
+      {/* menu */}
+      <Menu
+        defaultSelectedKeys={["1"]}
+        defaultOpenKeys={["sub1"]}
+        mode="inline"
+        theme="dark"
+      >
+        <Menu.Item key="1" icon={<PieChartOutlined />}>
+          HOME
+        </Menu.Item>
+        <SubMenu key="sub1" icon={<MailOutlined />} title="Navigation One">
+          <Menu.Item key="5" icon={<MailOutlined />}>Categories</Menu.Item>
+          <Menu.Item key="6" icon={<MailOutlined />}>Products</Menu.Item>
+        </SubMenu>
+      </Menu>
+      {/* menu end */}
+    </div>
+  );
 };
 
 export default LeftNav;
