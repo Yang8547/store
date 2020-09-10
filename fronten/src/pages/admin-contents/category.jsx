c564a287a4280542ff4aa3a783630f9503dc42ef
yang <anyang47@hotmail.com>
2020-09-10 14:32:27 -0400

    get sub-category list and update cat list

--- fronten/src/pages/admin-contents/category.jsx
+++ fronten/src/pages/admin-contents/category.jsx
@@ -1,14 +1,38 @@
 import React, { useState, useEffect } from "react";
 import { Card, Button, Table, Space } from "antd";
-import { PlusOutlined } from "@ant-design/icons";
+import { PlusOutlined, ArrowRightOutlined } from "@ant-design/icons";
 import { reqCategorys } from "../../api";
+import LinkedButton from "../../components/linked-button";
 
 const Category = () => {
-  const [categorys, setCategorys] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [categorys, setCategorys] = useState([]); //category list
+  const [loading, setLoading] = useState(false); //loading status
+  const [parentId, setParentId] = useState("0"); //parent id will triger fetch data
+  const [parentName, setParentName] = useState(""); //parent name display on table header
+
+  //   update parentId then triger update
+  const showSubCategory = category => {
+    setParentName(category.name);
+    setParentId(category._id);
+  };
+
+  //   fetch category
+  const showCategorys = () => {
+    setParentId("0"); //   update parentId then triger update
+    setParentName("");
+  };
 
   //   card header
-  const title = "CATEGORY";
+  const title =
+    parentId == "0" ? (
+      "CATEGORY"
+    ) : (
+      <>
+        <LinkedButton onClick={showCategorys}>CATEGORY</LinkedButton>
+        <ArrowRightOutlined style={{ margin: "0 10px" }} />
+        <span>{parentName}</span>
+      </>
+    );
   const extra = (
     <Button type="primary">
       <PlusOutlined />
@@ -25,28 +49,36 @@ const Category = () => {
     {
       title: "Action",
       width: "30%",
-      render: () => (
+      render: record => (
         <Space size="middle">
           <a>Edit</a>
-          <a>Sub-Category</a>
+          <a onClick={() => showSubCategory(record)}>Sub-Category</a>
         </Space>
       )
     }
   ];
 
-  const data = categorys;
-
   useEffect(() => {
     setLoading(true);
-    reqCategorys("0").then(res => {
+    // console.log('parentId',parentId);
+    reqCategorys(parentId).then(res => {
       setCategorys(res.data);
       setLoading(false);
+      //   console.log(res.data);
     });
-  }, []);
+  }, [parentId]); // parentId update will cause fetch data
+
   return (
     <Card title={title} extra={extra}>
       {/* is dataSource[i].key not provide，use rowKey to asign the key */}
-      <Table loading={loading} pagination={{defaultPageSize:3,showQuickJumper:true}} rowKey="_id" bordered columns={columns} dataSource={data} />
+      <Table
+        loading={loading}
+        pagination={{ defaultPageSize: 3, showQuickJumper: true }}
+        rowKey="_id"
+        bordered
+        columns={columns}
+        dataSource={categorys}
+      />
     </Card>
   );
 };
