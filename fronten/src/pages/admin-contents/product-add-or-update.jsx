de25243d7a6a723a567973207b7497c157ae8974
yang <anyang47@hotmail.com>
2020-09-22 13:39:14 -0400

    form validation

--- fronten/src/pages/admin-contents/product-add-or-update.jsx
+++ fronten/src/pages/admin-contents/product-add-or-update.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { Card, Form, Input, Cascader, Button, message } from "antd";
 import { ArrowLeftOutlined } from "@ant-design/icons";
-import LinkedButton from "../../components/linked-button"
+import LinkedButton from "../../components/linked-button";
+import { number } from "prop-types";
 // form layout
 const formItemLayout = {
   labelCol: {
@@ -13,7 +14,11 @@ const formItemLayout = {
 };
 const { TextArea } = Input;
 
-const ProductAddOrUpdate = (props) => {
+const onFinish = values => {
+  console.log("Received values of form: ", values);
+};
+
+const ProductAddOrUpdate = props => {
   const title = (
     <span>
       <LinkedButton
@@ -33,15 +38,57 @@ const ProductAddOrUpdate = (props) => {
   return (
     <div>
       <Card title={title} className="product-add">
-        <Form {...formItemLayout}>
-          <Form.Item label="Product Name">
-          <Input placeholder="please enter product name" />
+        <Form {...formItemLayout} onFinish={onFinish}>
+          <Form.Item
+            label="Product Name"
+            name="name"
+            rules={[
+              {
+                required: true,
+                message: "Please enter product name"
+              }
+            ]}
+          >
+            <Input placeholder="please enter product name" />
           </Form.Item>
-          <Form.Item label="Product Description">
-          < TextArea autoSize={{ minRows: 2, maxRows: 6 }}/>
+          <Form.Item
+            label="Product Description"
+            name="desc"
+            rules={[
+              {
+                required: true,
+                message: "Please enter product description"
+              }
+            ]}
+          >
+            <TextArea autoSize={{ minRows: 2, maxRows: 6 }} />
           </Form.Item>
-          <Form.Item label="Product Price">
-          <Input  prefix="$" />
+          <Form.Item
+            label="Product Price"
+            name="price"
+            rules={[
+              {
+                required: true,
+                message: "Please enter product price"
+              },
+              {
+                type: "number",
+                message: "Price must be number"
+              },
+              {
+                validator: (_, value) =>
+                  value*1>0 ? Promise.resolve() : Promise.reject('Price must larger than 0'),
+              },
+            ]}
+          >
+            <Input prefix="$" />
+          </Form.Item>
+
+          {/* submit button */}
+          <Form.Item>
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
           </Form.Item>
         </Form>
       </Card>
